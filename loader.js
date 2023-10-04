54c4687a7aa12eb2c5376f28e724d03dd7d32689
Nguyen Quoc Hung <nguyenquochung.workvn@gmail.com>
2023-10-04 10:13:08 +0700

    fix

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
+
     var video = document.querySelector('#video');
     var sceneEl = document.getElementById('scene');
     let arSystem;
@@ -8,12 +8,24 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     sceneEl.addEventListener('arReady', function () {
         alert("Click vào màn hình để phát lại video");
-        sceneEl.addEventListener('touchstart', function () { 
+        sceneEl.addEventListener('touchstart', function () {
             //Fix the problem of video not play (A-frame)
-            video.pause();
-            video.currentTime = 0;
-            video.play();
-            alert("Video đang được phát lại");
+            if (video.paused == true) {
+                video.play();
+                alert("Video đang được phát lại");
+            } else {
+                video.pause();
+            }
+        });
+        window.addEventListener('click', function () {
+            //Fix the problem of video not play (A-frame)
+            if (video.paused == true) {
+                video.play();
+                alert("Video đang được phát lại");
+            } else {
+                video.pause();
+            }
+
         });
     });
 });
