3a10740679d4297a1163d5b17d2824485abd4e59
Nguyen Quoc Hung <nguyenquochung.workvn@gmail.com>
2023-10-04 09:59:29 +0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    alert("Click vào màn hình để phát lại video");
+    
     var video = document.querySelector('#video');
-    var scene = document.getElementById('scene');
-    scene.addEventListener('touchstart', function () { 
-        alert("Đang phát lại video");
-        video.play();
+    var sceneEl = document.getElementById('scene');
+    let arSystem;
+    sceneEl.addEventListener('loaded', function () {
+        arSystem = sceneEl.systems['mindar-image-system'];
+    });
+    sceneEl.addEventListener('arReady', function () {
+        alert("Click vào màn hình để phát lại video");
+        sceneEl.addEventListener('touchstart', function () { 
+            alert("Đang phát lại video");
+            video.play();
+        });
     });
 });
